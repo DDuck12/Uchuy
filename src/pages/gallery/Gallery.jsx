9697ccf69be9d903c356ee1ccebdbf8e0d043279
DDuck12 <diegoa1226@live.com>
2023-03-23 18:59:32 -0600

    Added Gallery page strcuture and data, fixed double footer bug

--- src/pages/gallery/Gallery.jsx
+++ src/pages/gallery/Gallery.jsx
@@ -1,8 +1,15 @@
 import './gallery.css'
+import Header from '../../components/Header'
+import HeaderImage from '../../images/background.jpg'
+import './gallery.css'
 
 function Gallery() {
   return (
-    <div>Gallery</div>
+    <>
+      <Header title="Galeria" image={HeaderImage}>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab mollitia quae obcaecati expedita commodi pariatur saepe qui alias ratione corporis!
+      </Header>
+    </>
   )
 }
 
