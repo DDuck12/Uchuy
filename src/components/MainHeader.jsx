77178b2902cc19b2aa01637b7190b071d365f6f3
DDuck12 <diegoa1226@live.com>
2023-03-28 22:13:32 -0600

    Changed home screen image and nav bar color

--- src/components/MainHeader.jsx
+++ src/components/MainHeader.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom"
-import Image from '../images/duckplaceholder.png'
+import Image from '../images/logow.png'
 
 const MainHeader = () => {
   return (
--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -15,7 +15,7 @@ function Contact() {
       <div className="container contact__container">
         <div className="contact__wrapper">
           <a href="mailto:Kroon.steel.design@gmail.com" target="_blank" rel="noreferrer noopener"><MdEmail/></a>
-          <a href="http://m.me/profile.php?id=100087619848903" target="_blank" rel="noreferrer noopener"><BsMessenger/></a>
+          <a href="http://m.me/jesus.a.medina.9" target="_blank" rel="noreferrer noopener"><BsMessenger/></a>
           <a href="https://wa.me/527221285022" target="_blank" rel="noreferrer noopener"><IoLogoWhatsapp/></a>
         </div>
       </div>
